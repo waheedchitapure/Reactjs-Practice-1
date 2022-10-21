94321f4eb96c20e666f63e995f89b449930a0a89
waheed <79761710+waheedchitapure@users.noreply.github.com>
2022-10-22 03:50:40 +0530

    update_1.0

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>APP </h1>
+
+      <h2>Total</h2>
     </div>
   );
 }
