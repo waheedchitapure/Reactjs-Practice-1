7e5b45fe4023da143e32e79fd5f523a2c4b704fe
waheed <79761710+waheedchitapure@users.noreply.github.com>
2022-10-28 18:14:55 +0000

    update_1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 
 export const App = () => {
+  const [counter, SetCounter] = useState(0);
+
+  const handleClick1 = () => {
+    SetCounter(counter + 1)
+  }
+
+  const handleClick2 = () => {
+    SetCounter(counter - 1)
+  }
   return (
     <>
       <div className="main">
@@ -11,12 +21,19 @@ export const App = () => {
 
           <h1>
             This is the Counter Example
-
           </h1>
 
-          <div className="buttons">
-            <button className="inc"></button>
-            <button className="dec"></button>
+          <h3 style={{ marginTop: "60px " }}>
+            {counter}
+          </h3>
+
+          <div
+            style={{
+              marginTop: '50px'
+            }}
+            className="buttons">
+            <button className="inc" onClick={handleClick1}> Increment</button>
+            <button className="dec" onClick={handleClick2}> Decrement</button>
 
           </div>
 
