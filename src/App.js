284f741eca24f3b90ec62ba8c8c2828042971b19
waheed <79761710+waheedchitapure@users.noreply.github.com>
2022-10-29 01:04:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,4 @@
-// import "./styles.css";
 
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>sadas</h1>
-//       <h2>
-//         This is Practice
-
-//       </h2>
-
-//     </div>
-//   );
-// }
 
 import "./styles.css";
 
